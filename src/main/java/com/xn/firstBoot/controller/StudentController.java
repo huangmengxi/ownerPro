3a1e22fdecf2d7e19b962ae29468ee7daaf0fb0d
huangmengxi <30612291+huangmengxi@users.noreply.github.com>
2018-10-22 17:55:19 +0800

    Update StudentController.java

--- src/main/java/com/xn/firstBoot/controller/StudentController.java
+++ src/main/java/com/xn/firstBoot/controller/StudentController.java
@@ -39,6 +39,7 @@ public class StudentController
     @RequestMapping(value="/insertStudentWithBackId",method=RequestMethod.POST)
     public Student insertStudentWithBackId(@RequestParam("sname") String sname, @RequestParam("sex") String sex){
         System.out.println("333");
+        System.out.println("444");
         return studentService.insertStudentWithBackId(sname, sex);
     }
 }
